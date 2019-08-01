ec9b7c17bce4e06106abb7b31855bbe46e23ffb1
thw <thwxiaozi@126.com>
2019-08-02 01:31:47 +0800

    自动生成代码 测试通过

---
+++ gulimall-pms/src/main/java/com/atguigu/gulimall/pms/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.atguigu.gulimall.pms.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.atguigu.gulimall.pms.entity.CategoryEntity;
+import com.atguigu.gulimall.commons.bean.PageVo;
+import com.atguigu.gulimall.commons.bean.QueryCondition;
+
+
+/**
+ * 商品三级分类
+ *
+ * @author leifengyang
+ * @email lfy@atguigu.com
+ * @date 2019-08-01 21:01:31
+ */
+public interface CategoryService extends IService<CategoryEntity> {
+
+    PageVo queryPage(QueryCondition params);
+}
+
